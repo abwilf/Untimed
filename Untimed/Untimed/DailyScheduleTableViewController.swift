bdd6a20d430e6d195b39d712d843dbd1e9cf43c7
Alex Wilf <abwilf@umich.edu>
2016-04-08 22:42:09 -0400

    in-array delete functionality now saves

--- Untimed/Untimed/DailyScheduleTableViewController.swift
+++ Untimed/Untimed/DailyScheduleTableViewController.swift
@@ -301,17 +301,20 @@ class DailyScheduleTableViewController: UITableViewController {
     }
     */
 
-    /*
-    // Override to support editing the table view.
+    
+    // delete from dailysched view
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+        if (editingStyle == UITableViewCellEditingStyle.Delete){
+            
+            // delete and save
+            taskManager.deleteTaskAtIndex(indexPath.row)
+            tableView.reloadData()
+            }
+        }
+    
     }
-    */
+
 
     /*
     // Override to support rearranging the table view.
@@ -338,4 +341,4 @@ class DailyScheduleTableViewController: UITableViewController {
     }
     */
 
-}
+
