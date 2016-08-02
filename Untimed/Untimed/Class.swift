6e3b316937e5c05c6d62d26eaae0514a39125a93
Keenan Tullis <keenanjt@umich.edu>
2016-08-02 10:51:49 -0400

    implemented code to persist Class properties

--- Untimed/Untimed/Class.swift
+++ Untimed/Untimed/Class.swift
@@ -13,4 +13,21 @@ class Class: Task {
     var projAndAssns: [Task] = []
     var tasksIndex: Int = 0
     
+    override init() {
+        super.init()
+    }
+    
+    override func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(title, forKey:"Title")
+        aCoder.encodeObject(projAndAssns, forKey: "Projects and Assignments")
+        aCoder.encodeInteger(tasksIndex, forKey: "tasksIndex")
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.projAndAssns = aDecoder.decodeObjectForKey("Projects and Assignments") as? [Task] ?? []
+        self.tasksIndex = aDecoder.decodeIntegerForKey("tasksIndex")
+        aDecoder.decodeObjectForKey("Title") as! String
+    }
+
 }
